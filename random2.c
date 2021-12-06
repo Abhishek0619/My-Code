65eac9679b5dd9845c349e05301632b85705eb0a
Abhishek0619 <abhisheksur619@gmail.com>
2021-12-06 15:35:55 +0530

    Second version

---
+++ random2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int num,rev_num=0,rem;
+	printf("Enter number:");
+	scanf("%d",&num);
+	int original_num=num;
+	while(num>0)
+	{
+		rem=num%10;
+		rev_num=rev_num*10+rem;
+		num/=10;
+	}
+	printf("The reverse number is %d\n",rev_num);
+	if(original_num==rev_num)
+		printf("The number is a palindrome");
+	else
+		printf("Not a palindrome");
+
+		printf("This file has been updated");
+				
+}
+	
